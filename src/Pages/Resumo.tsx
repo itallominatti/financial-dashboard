61819843534761002834aa21fe1ab3b3b878f9f7
itallominatti <itallominatti@gmail.com>
2024-09-10 08:40:39 -0300

    [ feat ] main -> estilos iniciais da lista de vendas

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -1,15 +1,44 @@
-import React from "react";
 import { useData } from "../Context/DataContext";
 
-
 const Resumo = () => {
-
     const { data } = useData();
+    if (data === null) return null;
+
+    const totalVendas = data.reduce((acc, item) => acc + item.preco, 0).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
+
+    const totalRecebido = data.reduce((acc, item) =>
+        acc + (item.status === 'pago' ? item.preco : 0), 0).toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+        });
+
+    const totalProcessando = data.reduce((acc, item) => acc + (item.status === 'processando' ? item.preco : 0), 0).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
 
     return (
-
-        <div>Resumo</div>
-    )
+        <section>
+            <div className="resumo flex mb">
+                <div className="box">
+                    <h2>Vendas</h2>
+                    <span>{totalVendas}</span>
+                </div>
+                <div className="box">
+                    <h2>Recebido</h2>
+                    <span>{totalRecebido}</span>
+                </div>
+                <div className="box">
+                    <h2>Processando</h2>
+                    <span>{totalProcessando}</span>
+                </div>
+            </div>
+            <div className="box mb">Gráficos</div>
+        </section>
+    );
 }
 
 export default Resumo;
